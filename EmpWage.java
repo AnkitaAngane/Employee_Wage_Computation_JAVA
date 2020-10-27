f6980633aa889b129f50c070f71b4c0b9849d78a
Ankita <Ankitaangane20@gmail.com>
2020-10-27 22:26:58 +0530

    Added welcome message to display

---
+++ EmpWage.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+public class EmpWage{
+
+	public static void main(String args[]){
+		System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM!");
+
+	}
+
+}
