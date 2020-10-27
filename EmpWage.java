0bd72441401053ccc7de59016e4430032f023ff0
Ankita <Ankitaangane20@gmail.com>
2020-10-27 23:20:02 +0530

    Added monthly wage for an employee

--- EmpWage.java
+++ EmpWage.java
@@ -9,30 +9,45 @@ public class EmpWage{
 		int empRatePerHr=20;
 		int empHr;
 		int dailyWage;
+		int noWorkingDays=20;
+		int totalSalary=0;
+		int partTime=0;
+		int fullTime=0;
+		int absent=0;
 
 		System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM!");
 
 		//UC1: Employee check for attendance
-		Random ran=new Random();
-                int ran1=ran.nextInt(3)+1;
-                //System.out.println("Number is : "+ran1);
-		switch(ran1){
-			case 3 :
-				System.out.println("Employee is absent");
-				break;
-			case 1 :
-				System.out.println("Employee is Present for full time");
-				empHr=8;
-				dailyWage=empRatePerHr*empHr;
-				System.out.println("Daily Employee Wage is "+dailyWage);
-				break;
-			default :
-				System.out.println("Employee is Present for part time");
-                        	empHr=4;
-                        	dailyWage=empRatePerHr*empHr;
-                        	System.out.println("Daily Employee Wage is "+dailyWage);
+
+		for(int counter=1; counter<=noWorkingDays; counter++){
+
+			Random ran=new Random();
+        	        int ran1=ran.nextInt(3)+1;
+                	//System.out.println("Number is : "+ran1);
+			switch(ran1){
+				case 3 :
+					//System.out.println("Employee is absent");
+					absent+=1;
+					empHr=0;
+					break;
+				case 1 :
+					//System.out.println("Employee is Present for full time");
+					fullTime+=1;
+					empHr=8;
+					break;
+				default :
+					//System.out.println("Employee is Present for part time");
+                        		partTime+=1;
+					empHr=4;
+			}
+		dailyWage=empRatePerHr*empHr;
+		totalSalary=dailyWage+totalSalary;
+
 		}
 
+		System.out.println("EMPLOYEE DATA :");
+		System.out.println("FullTime Present Days : "+fullTime+" Part Time Present Days : "+partTime+" Absent Days : "+absent);
+		System.out.println("Total Salary for a Month is "+totalSalary);
 	}
 
 }
