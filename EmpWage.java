5b9b031e49ee3914329e3d3dfdf81b507cb8edf6
Ankita <Ankitaangane20@gmail.com>
2020-11-02 17:30:10 +0530

    Added code for multiple Companies different wage, working hour and working days

--- EmpWage.java
+++ EmpWage.java
@@ -3,23 +3,24 @@ import java.io.*;
 
 public class EmpWage{
 
-	public void WorkingHr(){
+	public static void WorkingHr(int Wage, int WorkingDay, int Hour){
 
 	//Variable declaration
-		int empRatePerHr=20;
+		int empRatePerHr=Wage;
 		int empHr;
 		int dailyWage;
-		int noWorkingDays=20;
+		int noWorkingDays=WorkingDay;
 		int totalSalary=0;
 		int partTime=0;
 		int fullTime=0;
 		int absent=0;
-		int maxHr=100;
+		int maxHr=Hour;
 		int totalDays=0;
 		int totalEmpHr=0;
 
 		System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM!");
-
+		System.out.println("COMPANY DATA");
+		System.out.println("Employee Wage per hour "+empRatePerHr+" Maximum Working Days "+maxHr+" Maximum Working Hours "+maxHr);
 		//UC1: Employee check for attendance
 
 		while( totalEmpHr<maxHr && totalDays<noWorkingDays ){
@@ -54,14 +55,16 @@ public class EmpWage{
 		System.out.println("FullTime Present Days : "+fullTime+" Part Time Present Days : "+partTime+" Absent Days : "+absent);
 		System.out.println("Total Working Days : "+totalDays+" Total Working Hours : "+totalEmpHr);
 		System.out.println("Total Salary for a Month is "+totalSalary);
+		System.out.println("");
 
 	}
 
 	public static void main(String args[]){
 
-	EmpWage obj = new EmpWage();
-	obj.WorkingHr();
-
+	//EmpWage company1 = new EmpWage();
+	WorkingHr(20,20,100);
+	WorkingHr(30,15,70);
+	WorkingHr(15,25,90);
 	}
 
 }
